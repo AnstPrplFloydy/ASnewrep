3d23ea1c9150502199e11375f073092ee9f55874
Anastasiya Skotnikova <floyd420@yandex.ru>
2024-08-05 18:30:09 +0300

    добавление функции, которая возвращает дату

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,20 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
+from datetime import datetime
 
 
 def mask_account_card(nums: str) -> str:
+    """Функция, маскирующая номер счета или карты"""
     if "Счет" in nums:
-
-        return get_mask_account(nums[5:])
+        account_mask = get_mask_account(nums[5:])
+        return f"{nums[:4]} {account_mask}"
 
     else:
+        card_mask = get_mask_card_number(nums[-16:])
+        return f"{nums[:-17]} {card_mask}"
 
-        return get_mask_card_number(nums[-16:])
 
+def get_date(string_with_date: str) -> str:
+    """Функция, возвращающая дату"""
+    searched_date = datetime.strptime(string_with_date, "%Y-%m-%dT%H:%M:%S.%f")
+    return searched_date.strftime("%d.%m.%Y")
