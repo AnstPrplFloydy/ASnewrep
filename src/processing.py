2cba3c21122c692114510b33042b1d2ecab4b149
Anastasiya Skotnikova <floyd420@yandex.ru>
2024-08-09 14:51:27 +0300

    добавлена функция sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,9 @@ def filter_by_state(
     """Функиия, возвращающая список словарей по значению ключа, переданного в аргумент"""
     return [x for x in list_of_dicts if x.get("state") == state]
 
+
+def sort_by_date(
+    list_of_dicts: List[Dict[str, Union[int, str]]], reverse: bool = True
+) -> List[Dict[str, Union[int, str]]]:
+    """Функция, принимающая список словарей и параметр для его сортировки по значению ключа"""
+    return sorted(list_of_dicts, key=lambda list_of_dicts: list_of_dicts["date"], reverse=reverse)
